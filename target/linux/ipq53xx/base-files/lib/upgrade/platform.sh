96dbb8ff2c50acdc4cc6398a1ed017d852c0fa31
Madhurya Kandamuru <quic_madhurya@quicinc.com>
2023-06-09 13:37:08 +0530

    target: ipq53xx adding mi04.1-c2 board support
    adding board support for qcom,ipq5332-ap-mi04.1-c2
    
    Change-Id: I987a8fd15f93814a2482d9f9624f4a1584d740de
    Signed-off-by: Madhurya Kandamuru <quic_madhurya@quicinc.com>

--- target/linux/ipq53xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq53xx/base-files/lib/upgrade/platform.sh
@@ -574,6 +574,7 @@ platform_do_upgrade() {
 	qcom,ipq5332-ap-mi01.7 |\
 	qcom,ipq5332-ap-mi01.9 |\
 	qcom,ipq5332-ap-mi04.1 |\
+	qcom,ipq5332-ap-mi04.1-c2 |\
 	qcom,ipq5332-db-mi01.1 |\
 	qcom,ipq5332-db-mi02.1)
 		for sec in $(print_sections $1); do
