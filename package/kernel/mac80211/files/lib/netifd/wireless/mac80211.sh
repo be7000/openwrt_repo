be7000/openwrt_repo

commit 2b1dbf76d9431a0e5abdab71553417c84078bfcb

mac80211.sh: Add wifi state support for synchronization
    wifi state:
    0 - wifi in down/not started/disabled
    1 - wifi in progress
    2 - wifi in up state/completed

    Add provision to check wifi state if any other module calls wifi
    or network restart. During wifi, if any module triggers wifi or
    network restart, then vap failure issue is observed due to the
    synchronization miss  between those modules. To avoid this, add
    wifi state support.

Change-Id: Ib9257bb22279d35bae6b41e48e127b4606923db6
Signed-off-by: Balamurugan Ramar <quic_bramar@quicinc.com>

package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -6,6 +6,7 @@
 
 init_wireless_driver "$@"
 MLD_VAP_DETAILS="/lib/netifd/wireless/wifi_mld_cfg.config"
+WIFI_STATE_FILE="/var/run/wifi_state"
 
 MP_CONFIG_INT="mesh_retry_timeout mesh_confirm_timeout mesh_holding_timeout mesh_max_peer_links
 	       mesh_max_retries mesh_ttl mesh_element_ttl mesh_hwmp_max_preq_retries
@@ -1916,6 +1917,14 @@ drv_mac80211_cleanup() {
 	hostapd_common_cleanup
 }
 
+drv_mac80211_radio_count() {
+        local config="$1"
+        local count="$2"
+
+        config_get disabled "$config" disabled
+        [ "$disabled" -eq 0 ] && radio_up_count=$((radio_up_count+1))
+}
+
 drv_mac80211_setup() {
 	local device=$1
 	# Note: In case of single wiphy, the device name would be radio#idx_band#bid
@@ -1959,6 +1968,8 @@ drv_mac80211_setup() {
 		wireless_set_retry 1
 		return 1
 	fi
+
+	[ -f $WIFI_STATE_FILE ] && echo 1 > $WIFI_STATE_FILE
 
 	if [ $(cat /sys/module/ath12k/parameters/ppe_rfs_support) == 'Y' ]; then
 		# Note: ppe_vp_accel and ppe_vp_rfs are mutually exclusive.
@@ -2138,17 +2149,23 @@ drv_mac80211_setup() {
 			dev_wlan="wlan${phy:0-1}"
 		fi
 
+		touch /var/run/hostapd-$device-updated-cfg
+		hostapd_cfg_updated=$(ls /var/run/hostapd-*-updated-cfg | wc -l)
+
 		if [ -z "$is_sphy_mband" ] || [ "$hostapd_add_bss" -eq 1 ]; then
 			[ -f "/var/run/hostapd-$dev_wlan.lock" ] && rm /var/run/hostapd-$dev_wlan.lock
 			# let hostapd manage interface $dev_wlan
 			hostapd_cli -iglobal raw ADD bss_config=$dev_wlan:$hostapd_conf_file
 			touch /var/run/hostapd-$dev_wlan.lock
+
+			config_foreach drv_mac80211_radio_count wifi-device
+			if [ "$hostapd_cfg_updated" = "$radio_up_count" ]; then
+				[ -f $WIFI_STATE_FILE ] && echo 2 > $WIFI_STATE_FILE
+			fi
 		else
 			if [ -f "/var/run/wifi-$phy.pid" ]; then
 				return
 			fi
-			touch /var/run/hostapd-$device-updated-cfg
-			hostapd_cfg_updated=$(ls /var/run/hostapd-*-updated-cfg | wc -l)
 		
 			if [ "$hostapd_cfg_updated" = "$radio_up_count" ]; then
 				bands_info=$(ls /var/run/hostapd*updated-cfg | grep -o band.)
@@ -2159,6 +2176,8 @@ drv_mac80211_setup() {
 				#MLO vaps, single instance of hostapd is started
 				/usr/sbin/hostapd -B -P /var/run/wifi-"$phy".pid $config_files
 				ret="$?"
+
+				[ -f $WIFI_STATE_FILE -a "$ret" -eq 0 ] && echo 2 > $WIFI_STATE_FILE
 
 				if [ "$band" = "5g" ]; then
 					interf_dfs="$(cat /var/run/hostapd-${phy}_band${device:11:1}.conf | grep interface | grep wlan | cut -d'=' -f 2 )"
@@ -2313,6 +2332,7 @@ drv_mac80211_teardown() {
 		echo "Bug: PHY is undefined for device '$1'"
 		return 1
 	}
+	[ -f $WIFI_STATE_FILE ] && echo 0 > $WIFI_STATE_FILE
 	device=$1
 	mac80211_interface_cleanup "$phy" "$1"
 	uci -q -P /var/state revert wireless.${device}

package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -3,6 +3,11 @@
 append DRIVERS "mac80211"
 
 MLD_VAP_DETAILS="/lib/netifd/wireless/wifi_mld_cfg.config"
+WIFI_STATE_FILE="/var/run/wifi_state"
+
+[ -f $WIFI_STATE_FILE ] || touch $WIFI_STATE_FILE
+state=$(cat $WIFI_STATE_FILE)
+[ -z "$state" -a -f $WIFI_STATE_FILE ] && echo 0 > $WIFI_STATE_FILE
 
 configure_service_param() {
 	enable_service=$2

commit 7e260b8aa2a70a5463fd516743d5bb6097f4e1b5

mac80211.sh: Fix for the vap up failure issue.
hostapd is not started due to the wrong assumption of
radio count in dual phy platform. Add separate count
variable for phy0 and phy1 to fix this issue.

Fixes: 2b1dbf76d9431 (Add wifi state support for sync)

Change-Id: I9ec8a0a7537b43c103fa0188128bd37f3cc17df4
Signed-off-by: Balamurugan Ramar <quic_bramar@quicinc.com>

package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
@@ -2169,9 +2169,10 @@ drv_mac80211_setup() {
 				if [ -f "/var/run/wifi-$phy.pid" ]; then
 					return
 				fi
-		
+				touch /var/run/hostapd-mld-$device-updated-cfg
+				hostapd_cfg_updated=$(ls /var/run/hostapd-mld-*-updated-cfg | wc -l)
 				if [ "$hostapd_cfg_updated" = "$radio_up_count" ]; then
-					bands_info=$(ls /var/run/hostapd*updated-cfg | grep -o band.)
+					bands_info=$(ls /var/run/hostapd-mld-*-updated-cfg | grep -o band.)
 					for __band in $bands_info
 					do
 						append  config_files /var/run/hostapd-phy${phy#phy}_${__band}.conf